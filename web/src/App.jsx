a6e14b8cc56d513d8563eb5c06571d45f403c1d9
JFmaia <jfmaia741@gmail.com>
2022-06-01 16:39:35 -0300

    feat: Modificando.
    Atualizando o App.js e retirando o export defalt

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,1 +1,17 @@
-
+export const App = () => {
+  return (
+    <div id="tweet">
+      <div id="col-avatar" className="col">
+        <img src="/src/avatar.png" className="tweet_img"/>
+      </div>
+      <div id="col-text" className="col">
+        <span>Elon Musck</span>
+        <span>@elonmusck</span>
+        <p>
+          Let's make Twitter maximun fun!
+        </p>
+      </div>
+    </div>
+  );
+}
+  
