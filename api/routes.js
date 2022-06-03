9fd8508ed2252790b6382b49cd546fd749e4994b
JFmaia <jfmaia741@gmail.com>
2022-06-03 11:33:05 -0300

    Resetando as funcionalidades das rotas.

--- api/routes.js
+++ api/routes.js
@@ -2,4 +2,17 @@ import Router from '@koa/router' //Rotas da api
 
 export const router = new Router();
 
-router.get('/tweets', ()=> {});
+router.get('/tweets', ctx => {
+    
+});
+
+router.post('/tweets', ctx => {
+   
+});
+
+/*Info
+
+    Body conteudo da request.
+    Query é o que vem na url da request.
+
+*/ 
