becf909491649f45252a4f2bfb451ce33df02871
JFmaia <jfmaia741@gmail.com>
2022-06-02 21:41:58 -0300

    api/index.js

--- api/index.js
+++
@@ -1,9 +0,0 @@
-import Koa from 'koa' //Servidor Http
-
-const app = new Koa();
-
-app.use(ctx =>{
-    ctx.body = 'hello world'
-});
-
-app.listen(9901, ()=> console.log("Server is running at 9901"));
