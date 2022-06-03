eabb24cfa766ea6840a8311af20caeffa3c85acd
JFmaia <jfmaia741@gmail.com>
2022-06-03 18:18:03 -0300

    Modificando o setup.

--- api/setup.js
+++ api/setup.js
@@ -1,7 +1,9 @@
 import Koa from 'koa';
-import { router } from './routes';
+import { router } from './routes.js';
+import bodyParser from 'koa-bodyparser';
 
 export const app = new Koa();
 
+app.use(bodyParser())
 app.use(router.routes());
 app.use(router.allowedMethods());
