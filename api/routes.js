25c83fb498f8ae669cc65a5009f5292a8a94a293
JFmaia <jfmaia741@gmail.com>
2022-06-03 22:30:18 -0300

    Modificando as rotas, implementando a logica com o banco de dados.

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,24 @@
 import Router from '@koa/router';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 export const router = new Router();
 
-router.get('/tweets', ctx =>{
-    
+
+router.get('/tweets', async ctx =>{
+    const tweets = await prisma.tweet.findMany();
+    ctx.body = tweets;
 })
 
-router.post('/tweets', ctx =>{
+router.post('/tweets', async ctx =>{
+   const tweet = {
+        userId: 'cl3z6c6ik002534td2kzd7n70',
+        text: ctx.request.body.text
+   }
 
+   const doc = await prisma.tweet.create({
+       data: tweet,
+   })
+
+   ctx.body = doc;   
 })
