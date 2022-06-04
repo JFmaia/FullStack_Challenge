02b1c9c8222053b5f3518c21db0a84f7afc0da28
JFmaia <jfmaia741@gmail.com>
2022-06-04 14:09:06 -0300

    Tratando erros da rota signup.

--- api/routes.js
+++ api/routes.js
@@ -31,20 +31,32 @@ router.post('/signup', async ctx =>{
     const saltRounds = 10;
     const password = bcrypt.hashSync(ctx.request.body.password, saltRounds); //criptografa a senha
     
-    const user = await prisma.user.create({
-        data:{
-            name: ctx.request.body.name,
-            username:ctx.request.body.username,
-            email: ctx.request.body.email,
-            password: password,
+    try {
+        const user = await prisma.user.create({
+            data:{
+                name: ctx.request.body.name,
+                username:ctx.request.body.username,
+                email: ctx.request.body.email,
+                password: password,
+            }
+        });
+
+        ctx.body = {
+            id: user.id,
+            name: user.name,
+            username: user.username,
+            email: user.email,
+        };
+    
+    } catch (error) {
+        if(error.meta && !error.meta.target){
+            ctx.status = 422
+            ctx.body = "Email ou nome de usuario já existe!";
+            return 
         }
-    });
 
-    ctx.body = {
-        id: user.id,
-        name: user.name,
-        username: user.username,
-        email: user.email,
-    };
-
+        ctx.status = 500;
+        ctx.body = "Internal server error";
+    }
+    
 })
