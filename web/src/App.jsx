d9c6dcb1fdc7e3579d53b700de67c7e7a8fe76aa
JFmaia <jfmaia741@gmail.com>
2022-06-05 13:35:27 -0300

    Salvando o estado do usuario.
    Ao ver que o usuário logou é salva essa informação na memoria e faz ele acessar a pagina de home.

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,8 @@
 import { Home } from './Home';
 import { Login } from './Login';
+import { useState } from 'react';
+
 export function App() {
-  return (
-    <Login />
-  );
+  const [user, setUser]= useState();
+  return user ? <Home/> : <Login signInUser = {setUser} /> ;
 }
