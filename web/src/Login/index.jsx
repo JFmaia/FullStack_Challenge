22be383d12ab1a52240eff43ac149ff0cd1647c1
JFmaia <jfmaia741@gmail.com>
2022-06-07 20:13:41 -0300

    feat: Atualização do login.

--- web/src/Login/index.jsx
+++ web/src/Login/index.jsx
@@ -38,53 +38,59 @@ export function Login({signInUser}) {
     });
 
     return(
-        <div className="h-full flex flex-col justify-center p-12 space-y-6">
-            <h1 className="text-3xl">Acesse sua conta</h1>
+        <div className='h-full flex justify-center '>
+            <div className='bg-birdBlue lg:flex-1'></div>
+            <div className="flex-1 flex justify-center items-center p-16 space-y-6">
+                <div className='max-w-md flex-1 space-y-6'>
+                    <h1 className="text-3xl">Acesse sua conta</h1>
+                    <form className="space-y-6" onSubmit={formik.handleSubmit}>
+                        <div className='space-y-2'>
+                            <Input 
+                                type='text'
+                                name='email' 
+                                placeholder="E-mail"
+                                value= {formik.values.email}
+                                onChange={formik.handleChange}// para pegar o que foi digitado no input
+                                onBlur={formik.handleBlur}// evento que será feito a verificação do campo
+                                disabled={formik.isSubmitting}// para desabilitar o campo enquanto o formulário estiver sendo enviado
+                            />
+                            {(formik.touched.email && formik.errors.email) && ( 
+                                <div className='text-red-500 pl-2'>{formik.errors.email}</div>
+                            )}
+                        </div>
 
-            <form className="space-y-6" onSubmit={formik.handleSubmit}>
-                <div className='space-y-2'>
-                    <Input 
-                        type='text'
-                        name='email' 
-                        placeholder="E-mail"
-                        value= {formik.values.email}
-                        onChange={formik.handleChange}// para pegar o que foi digitado no input
-                        onBlur={formik.handleBlur}// evento que será feito a verificação do campo
-                        disabled={formik.isSubmitting}// para desabilitar o campo enquanto o formulário estiver sendo enviado
-                    />
-                    {(formik.touched.email && formik.errors.email) && ( 
-                        <div className='text-red-500 pl-2'>{formik.errors.email}</div>
-                    )}
+                        <div className='space-y-2'>
+                            <Input 
+                                type = 'password'
+                                name='password' 
+                                placeholder="Senha"
+                                value= {formik.values.password}
+                                onChange={formik.handleChange}// para pegar o que foi digitado no input
+                                onBlur={formik.handleBlur}// evento que será feito a verificação do campo
+                                disabled={formik.isSubmitting}// para desabilitar o campo enquanto o formulário estiver sendo enviado
+                            />
+                            {(formik.touched.password && formik.errors.password) && ( 
+                                <div className='text-red-500 pl-2'>{formik.errors.password}</div>
+                            )}
+                        </div>
+
+                        <button 
+                            type="submit"
+                            className="w-full bg-birdBlue py-4 rounded-full disabled:opacity-50 text-lg" 
+                            disabled={formik.isSubmitting || !formik.isValid }
+                        >
+                            {formik.isSubmitting ? 'Enviando...': 'Entrar'}
+                        </button>
+                    
+                    </form>
+                    <div className='text-center'>
+                        <span className="text-sm text-silver">
+                            Não tem conta? 
+                            <a className="text-birdBlue" href="/signup"> Cadastre-se.</a>
+                        </span>
+                    </div>
                 </div>
-
-                <div className='space-y-2'>
-                    <Input 
-                        type = 'password'
-                        name='password' 
-                        placeholder="Senha"
-                        value= {formik.values.password}
-                        onChange={formik.handleChange}// para pegar o que foi digitado no input
-                        onBlur={formik.handleBlur}// evento que será feito a verificação do campo
-                        disabled={formik.isSubmitting}// para desabilitar o campo enquanto o formulário estiver sendo enviado
-                    />
-                    {(formik.touched.password && formik.errors.password) && ( 
-                        <div className='text-red-500 pl-2'>{formik.errors.password}</div>
-                    )}
-                </div>
-
-                <button 
-                    type="submit"
-                    className="w-full bg-birdBlue py-4 rounded-full disabled:opacity-50 text-lg" 
-                    disabled={formik.isSubmitting || !formik.isValid }
-                >
-                   {formik.isSubmitting ? 'Enviando...': 'Entrar'}
-                </button>
-                
-            </form>
-            <span className="text-sm text-silver text-center">
-                Não tem conta? 
-                <a className="text-birdBlue" href="/signup"> Cadastre-se.</a>
-            </span>
+            </div>
         </div>
-    )
+    );
 }
